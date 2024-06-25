8b10c99ae99755a9cb4aaa8beecbef7feb4964e5
TawaGC <giucalvia99@gmail.com>
2024-06-25 19:00:26 +0200

    .

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Blog, Post } from '../../models/blog';
 import { PostsService } from '../../services/posts.service';
 
@@ -7,11 +7,14 @@ import { PostsService } from '../../services/posts.service';
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit{
+
   posts: Post[] = []
+
   constructor(private ps : PostsService){ }
 
+  //Funzione imlementata da angular non appena caricato il componente
   ngOnInit(){
-    this.posts = this.ps.getPost()
+    this.posts = this.ps.getPost();
   }
 }
