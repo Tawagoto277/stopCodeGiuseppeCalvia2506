f1d9645d5c8cac7891dee8670cc4a32e6a305d8c
TawaGC <giucalvia99@gmail.com>
2024-06-25 11:41:32 +0200

    inizio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreferitiComponent } from './components/preferiti/preferiti.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreferitiComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
